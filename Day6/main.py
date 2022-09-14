24dd0d6181b45ad3fc9f1262371bf4bf391f94ff
Jawad Ntefeh <73185344+Jawad-Ntefeh@users.noreply.github.com>
2022-09-14 20:10:52 +0300

    Update main.py

--- Day6/main.py
+++ Day6/main.py
@@ -1,3 +1,5 @@
+# Day 6 Project : Reeborg's World "Maze" Level Solutions
+
 # Specific Solution
 # def turn_right():
 #     turn_left()
@@ -14,17 +16,17 @@
 #         turn_left()
 
 
-# General Solution
-# def turn_right():
-#     turn_left()
-#     turn_left()
-#     turn_left()
-#
-# while not at_goal():
-#     if right_is_clear() and not front_is_clear():
-#         turn_right()
-#         move()
-#     elif front_is_clear():
-#         move()
-#     else:
-#         turn_left()
+General Solution
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear() and not front_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
