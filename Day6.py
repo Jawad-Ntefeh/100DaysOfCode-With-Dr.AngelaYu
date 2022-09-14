5b9087d21473e8071c49248a3eaa55fbb21a3673
Jawad Ntefeh <73185344+Jawad-Ntefeh@users.noreply.github.com>
2022-09-14 19:48:34 +0300

    Create Day6.py
    Solution to Reborg world's "Maze" level

---
+++ Day6.py
@@ -0,0 +1,30 @@
+# Specific Solution
+# def turn_right():
+#     turn_left()
+    #     turn_left()
+#     turn_left()
+#
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+
+
+# General Solution
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# while not at_goal():
+#     if right_is_clear() and not front_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
