80b4f231a785012f2f4edc656ba530a9a15d0eea
Jawad Ntefeh <73185344+Jawad-Ntefeh@users.noreply.github.com>
2022-09-14 20:03:20 +0300

    Delete Day1.py

--- Day1.py
+++
@@ -1,8 +0,0 @@
-# Day 1 Project: Band Name Generator
-
-print("Welcome to the band name generator")
-print("What's the name of the city you grew up in?")
-city_name = input()
-print("What's your pet's name?")
-pet_name = input()
-print("Your band name could be " + city_name + " " + pet_name)
