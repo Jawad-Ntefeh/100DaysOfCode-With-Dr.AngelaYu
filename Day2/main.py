37ed39551965095984f5dd2b6a0da4d1f3af9d77
Jawad Ntefeh <73185344+Jawad-Ntefeh@users.noreply.github.com>
2022-09-14 20:07:30 +0300

    Update main.py

--- Day2/main.py
+++ Day2/main.py
@@ -1,4 +1,4 @@
-# Day 2 Project: Tip Calculator
+# Day 2 Project : Tip Calculator
 print('Welcome to the tip calculator')
 total_bill = float(input('What was the total bill? $'))
 percentage_tip = int(input('What percentage tip would you like to give? 10, 12, or 15? '))
