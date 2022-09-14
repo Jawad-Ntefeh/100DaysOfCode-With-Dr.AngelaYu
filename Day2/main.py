5ed3d02caf0822f656afdaaab8ae1a008a4a94f5
Jawad Ntefeh <73185344+Jawad-Ntefeh@users.noreply.github.com>
2022-09-14 20:04:19 +0300

    Update main.py

--- Day2/main.py
+++ Day2/main.py
@@ -1,12 +1,3 @@
-# import cmath
-# print(3*(3+3)/3-3)
-
-#BMI Calculator
-# height = float(input('Enter your height in m: '))
-# weight = float(input('Enter your weight in kg: '))
-# bmi = "{:.2f}".format((weight/height**2))
-# print(f'Your BMI is {bmi}')
-
 # Day 2 Project: Tip Calculator
 print('Welcome to the tip calculator')
 total_bill = float(input('What was the total bill? $'))
